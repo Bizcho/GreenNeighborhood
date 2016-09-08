cd014389b77241b35c8800d4868977f7c9980df8
Daisy Alba <dga@transtelco.netm>
2016-09-08 09:42:32 -0600

    scaffolds

--- GreenNeighborhood/config/routes.rb
+++ GreenNeighborhood/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :communities
+  resources :events
+  resources :comments
+  resources :topics
+  resources :services
+  resources :parks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
