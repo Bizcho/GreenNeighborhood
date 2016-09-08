56336d6bcabe378768fdb40487f5f1ac51d4b80b
Luis Valdez <bizchov@gmail.com>
2016-09-08 12:10:25 -0600

    merging

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,15 @@ Rails.application.routes.draw do
   resources :services
   resources :parks
 
+  namespace :forum do
+    resources :topics do
+      collection do 
+        get 'index'
+      end
+    end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
