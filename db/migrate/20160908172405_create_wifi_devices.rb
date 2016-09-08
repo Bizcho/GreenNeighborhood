bd347f3b88122648fd67a6cf17b8d47b5ff13008
Ruben Gomez <rgg@transtelco.net>
2016-09-08 11:56:13 -0600

    fixed a bug

--- db/migrate/20160908172405_create_wifi_devices.rb
+++ db/migrate/20160908172405_create_wifi_devices.rb
@@ -1,7 +1,7 @@
 class CreateWifiDevices < ActiveRecord::Migration
   def change
     create_table :wifi_devices do |t|
-      t.int :park_id
+      t.integer :park_id
       t.boolean :alerted
 
       t.timestamps null: false
